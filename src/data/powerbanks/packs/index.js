566f90d3bc047ea3ab5cfc6ea1033ba8f1ad461d
aaron yim <aaronchyim@gmail.com>
2025-12-09 15:54:32 -0600

    data: add per-pack entries from charging sheet (>65W, >10k mAh)

--- src/data/powerbanks/packs/index.js
+++ src/data/powerbanks/packs/index.js
@@ -1,35 +1,155 @@
+import 2_modell from './2-modell.json';
+import amegat_27_600_mah_140w_99_3wh from './amegat-27-600-mah-140w-99-3wh.json';
+import amegat_flaches_design_20_000_mah_100w_74wh from './amegat-flaches-design-20-000-mah-100w-74wh.json';
+import amegat_integriertes_usb_c_kabel_20_000_mah_130w_74wh from './amegat-integriertes-usb-c-kabel-20-000-mah-130w-74wh.json';
 import anker_165w from './anker-165w.json';
+import anker_2x_integrierte_usb_c_kabel_25_000_mah_165w_90wh from './anker-2x-integrierte-usb-c-kabel-25-000-mah-165w-90wh.json';
+import anker_737_24_000_mah_140w_86_4wh from './anker-737-24-000-mah-140w-86-4wh.json';
 import anker_737_24k from './anker-737-24k.json';
 import anker_87w from './anker-87w.json';
+import anker_prime_20_000_mah_200w_72wh from './anker-prime-20-000-mah-200w-72wh.json';
+import anker_prime_20_100_mah_220w_72_36wh from './anker-prime-20-100-mah-220w-72-36wh.json';
+import anker_prime_26_250_mah_300w_99_75wh from './anker-prime-26-250-mah-300w-99-75wh.json';
+import anker_prime_27_650_mah_250w_99_54wh from './anker-prime-27-650-mah-250w-99-54wh.json';
 import aohi_210w_10k from './aohi-210w-10k.json';
 import aohi_310w_20k from './aohi-310w-20k.json';
+import aohi_starship_20_000_mah_310w_74wh from './aohi-starship-20-000-mah-310w-74wh.json';
+import aohi_starship_27_600_mah_240w_99_3wh from './aohi-starship-27-600-mah-240w-99-3wh.json';
 import asperx_165w from './asperx-165w.json';
+import asperx_24_000_mah_145w_86wh from './asperx-24-000-mah-145w-86wh.json';
+import asperx_einziehbares_usb_c_kabel_20_000_mah_165w_72wh from './asperx-einziehbares-usb-c-kabel-20-000-mah-165w-72wh.json';
+import baseus_blade_20_000_mah_100w_74wh from './baseus-blade-20-000-mah-100w-74wh.json';
+import baseus_blade_hd_20_000_mah_100w_74wh from './baseus-blade-hd-20-000-mah-100w-74wh.json';
+import baseus_enercore_cr11_einziehbares_usb_c_kabel_20_000_mah_67w_72wh from './baseus-enercore-cr11-einziehbares-usb-c-kabel-20-000-mah-67w-72wh.json';
+import baseus_energeek_gr12_20_800_mah_145w_77wh from './baseus-energeek-gr12-20-800-mah-145w-77wh.json';
+import charmast_20_000_mah_100w_72wh from './charmast-20-000-mah-100w-72wh.json';
+import cuktech_10_plus_15_000_mah_120w_54wh from './cuktech-10-plus-15-000-mah-120w-54wh.json';
+import cuktech_15_20_000_mah_150w_74wh from './cuktech-15-20-000-mah-150w-74wh.json';
+import cuktech_15_air_15_000_mah_100w_58_32wh from './cuktech-15-air-15-000-mah-100w-58-32wh.json';
+import cuktech_15_se_20_000_mah_85w_74wh from './cuktech-15-se-20-000-mah-85w-74wh.json';
+import cuktech_15_ultra_20_000_mah_210w_72wh from './cuktech-15-ultra-20-000-mah-210w-72wh.json';
 import cuktech_15_ultra_20k from './cuktech-15-ultra-20k.json';
+import cuktech_20_25_000_mah_210w_90wh from './cuktech-20-25-000-mah-210w-90wh.json';
+import cuktech_25_25_000_mah_210w_90wh from './cuktech-25-25-000-mah-210w-90wh.json';
 import eco_flow_rapid_pro_20k from './eco-flow-rapid-pro-20k.json';
 import eco_flow_rapid_pro_276k from './eco-flow-rapid-pro-276k.json';
 import eco_flow_rapid_pro_x_276k from './eco-flow-rapid-pro-x-276k.json';
+import ecoflow_rapid_25_000_mah_170w_90wh from './ecoflow-rapid-25-000-mah-170w-90wh.json';
+import ecoflow_rapid_2x_integrierte_usb_c_kabel_25_000_mah_170w_90wh from './ecoflow-rapid-2x-integrierte-usb-c-kabel-25-000-mah-170w-90wh.json';
+import ecoflow_rapid_pro_einziehbares_usb_c_kabel_20_000_mah_230w_72wh from './ecoflow-rapid-pro-einziehbares-usb-c-kabel-20-000-mah-230w-72wh.json';
+import ecoflow_rapid_pro_einziehbares_usb_c_kabel_27_650_mah_300w_99_54wh from './ecoflow-rapid-pro-einziehbares-usb-c-kabel-27-650-mah-300w-99-54wh.json';
+import ecoflow_rapid_pro_x_einziehbares_usb_c_kabel_apple_watch_charger_27_650_mah_300w_99_3wh from './ecoflow-rapid-pro-x-einziehbares-usb-c-kabel-apple-watch-charger-27-650-mah-300w-99-3wh.json';
+import edge_smart_drive_x100_19_400_mah_315w_97wh from './edge-smart-drive-x100-19-400-mah-315w-97wh.json';
 import edge_x100_27k from './edge-x100-27k.json';
+import energyqc_energycore_120_27_000_mah_120w_99_9wh from './energyqc-energycore-120-27-000-mah-120w-99-9wh.json';
+import g_power_gt19_12_000_mah_176w_44_4wh from './g-power-gt19-12-000-mah-176w-44-4wh.json';
 import iniu_140w from './iniu-140w.json';
+import iniu_25_000_mah_100w_92_5wh from './iniu-25-000-mah-100w-92-5wh.json';
+import iniu_27_000_mah_140w_99_7wh from './iniu-27-000-mah-140w-99-7wh.json';
+import iniu_cougar_25_000_mah_100w_92_5wh from './iniu-cougar-25-000-mah-100w-92-5wh.json';
+import iniu_cougar_25_000_mah_140w_92_5wh from './iniu-cougar-25-000-mah-140w-92-5wh.json';
+import kooseed_27_000_mah_145w_97_2wh from './kooseed-27-000-mah-145w-97-2wh.json';
+import maxsure_integiriertes_usb_c_kabel_20_000_mah_145w_72wh from './maxsure-integiriertes-usb-c-kabel-20-000-mah-145w-72wh.json';
+import oneplus_supervooc_powerbank_20_000_mah_150w_72wh from './oneplus-supervooc-powerbank-20-000-mah-150w-72wh.json';
+import poweradd_pro_18_400_mah_130w_66_4wh from './poweradd-pro-18-400-mah-130w-66-4wh.json';
+import poweradd_pro_20_000_mah_100w_74wh from './poweradd-pro-20-000-mah-100w-74wh.json';
+import poweradd_pro_27_000_mah_140w_97_2wh from './poweradd-pro-27-000-mah-140w-97-2wh.json';
 import prime_250w_276k from './prime-250w-276k.json';
 import prime_300w_26k from './prime-300w-26k.json';
+import sdoutech_20_000_mah_180w_72wh from './sdoutech-20-000-mah-180w-72wh.json';
+import sharge_shargeek_140_20_000_mah_140w_72wh from './sharge-shargeek-140-20-000-mah-140w-72wh.json';
+import sharge_shargeek_170_24_000_mah_170w_86_4wh from './sharge-shargeek-170-24-000-mah-170w-86-4wh.json';
+import sharge_shargeek_300_24_000_mah_300w_86_4wh from './sharge-shargeek-300-24-000-mah-300w-86-4wh.json';
 import ugreen_165w from './ugreen-165w.json';
+import ugreen_hypercharge_intrigiertes_usb_c_kabel_20_000_mah_67w_72wh from './ugreen-hypercharge-intrigiertes-usb-c-kabel-20-000-mah-67w-72wh.json';
+import ugreen_nexode_12_000_mah_100w_44_4wh from './ugreen-nexode-12-000-mah-100w-44-4wh.json';
+import ugreen_nexode_20_000_mah_130w_72wh from './ugreen-nexode-20-000-mah-130w-72wh.json';
+import ugreen_nexode_25_000_mah_200w_92_5wh from './ugreen-nexode-25-000-mah-200w-92-5wh.json';
+import ugreen_nexode_einziehbares_usb_c_kabel_20_000_mah_165w_72wh from './ugreen-nexode-einziehbares-usb-c-kabel-20-000-mah-165w-72wh.json';
+import ugreen_nexode_inkl_qi2_20_000_mah_145w_72wh from './ugreen-nexode-inkl-qi2-20-000-mah-145w-72wh.json';
+import veger_integriertes_usb_c_lightning_kabel_25_000_mah_130w_92_5wh from './veger-integriertes-usb-c-lightning-kabel-25-000-mah-130w-92-5wh.json';
+import vention_27_000_mah_140w_99_3wh from './vention-27-000-mah-140w-99-3wh.json';
+import vention_integriertes_usb_c_kabel_20_000_mah_165w_74wh from './vention-integriertes-usb-c-kabel-20-000-mah-165w-74wh.json';
+import xdigi_20_000_mah_130w_72wh from './xdigi-20-000-mah-130w-72wh.json';
+import xiaomi_25_000_mah_212w_90_8wh from './xiaomi-25-000-mah-212w-90-8wh.json';
+import xtorm_nova_pro_20_000_mah_130w_72wh from './xtorm-nova-pro-20-000-mah-130w-72wh.json';
 
 const packs = [
+  2_modell,
+  amegat_27_600_mah_140w_99_3wh,
+  amegat_flaches_design_20_000_mah_100w_74wh,
+  amegat_integriertes_usb_c_kabel_20_000_mah_130w_74wh,
   anker_165w,
+  anker_2x_integrierte_usb_c_kabel_25_000_mah_165w_90wh,
+  anker_737_24_000_mah_140w_86_4wh,
   anker_737_24k,
   anker_87w,
+  anker_prime_20_000_mah_200w_72wh,
+  anker_prime_20_100_mah_220w_72_36wh,
+  anker_prime_26_250_mah_300w_99_75wh,
+  anker_prime_27_650_mah_250w_99_54wh,
   aohi_210w_10k,
   aohi_310w_20k,
+  aohi_starship_20_000_mah_310w_74wh,
+  aohi_starship_27_600_mah_240w_99_3wh,
   asperx_165w,
+  asperx_24_000_mah_145w_86wh,
+  asperx_einziehbares_usb_c_kabel_20_000_mah_165w_72wh,
+  baseus_blade_20_000_mah_100w_74wh,
+  baseus_blade_hd_20_000_mah_100w_74wh,
+  baseus_enercore_cr11_einziehbares_usb_c_kabel_20_000_mah_67w_72wh,
+  baseus_energeek_gr12_20_800_mah_145w_77wh,
+  charmast_20_000_mah_100w_72wh,
+  cuktech_10_plus_15_000_mah_120w_54wh,
+  cuktech_15_20_000_mah_150w_74wh,
+  cuktech_15_air_15_000_mah_100w_58_32wh,
+  cuktech_15_se_20_000_mah_85w_74wh,
+  cuktech_15_ultra_20_000_mah_210w_72wh,
   cuktech_15_ultra_20k,
+  cuktech_20_25_000_mah_210w_90wh,
+  cuktech_25_25_000_mah_210w_90wh,
   eco_flow_rapid_pro_20k,
   eco_flow_rapid_pro_276k,
   eco_flow_rapid_pro_x_276k,
+  ecoflow_rapid_25_000_mah_170w_90wh,
+  ecoflow_rapid_2x_integrierte_usb_c_kabel_25_000_mah_170w_90wh,
+  ecoflow_rapid_pro_einziehbares_usb_c_kabel_20_000_mah_230w_72wh,
+  ecoflow_rapid_pro_einziehbares_usb_c_kabel_27_650_mah_300w_99_54wh,
+  ecoflow_rapid_pro_x_einziehbares_usb_c_kabel_apple_watch_charger_27_650_mah_300w_99_3wh,
+  edge_smart_drive_x100_19_400_mah_315w_97wh,
   edge_x100_27k,
+  energyqc_energycore_120_27_000_mah_120w_99_9wh,
+  g_power_gt19_12_000_mah_176w_44_4wh,
   iniu_140w,
+  iniu_25_000_mah_100w_92_5wh,
+  iniu_27_000_mah_140w_99_7wh,
+  iniu_cougar_25_000_mah_100w_92_5wh,
+  iniu_cougar_25_000_mah_140w_92_5wh,
+  kooseed_27_000_mah_145w_97_2wh,
+  maxsure_integiriertes_usb_c_kabel_20_000_mah_145w_72wh,
+  oneplus_supervooc_powerbank_20_000_mah_150w_72wh,
+  poweradd_pro_18_400_mah_130w_66_4wh,
+  poweradd_pro_20_000_mah_100w_74wh,
+  poweradd_pro_27_000_mah_140w_97_2wh,
   prime_250w_276k,
   prime_300w_26k,
-  ugreen_165w
+  sdoutech_20_000_mah_180w_72wh,
+  sharge_shargeek_140_20_000_mah_140w_72wh,
+  sharge_shargeek_170_24_000_mah_170w_86_4wh,
+  sharge_shargeek_300_24_000_mah_300w_86_4wh,
+  ugreen_165w,
+  ugreen_hypercharge_intrigiertes_usb_c_kabel_20_000_mah_67w_72wh,
+  ugreen_nexode_12_000_mah_100w_44_4wh,
+  ugreen_nexode_20_000_mah_130w_72wh,
+  ugreen_nexode_25_000_mah_200w_92_5wh,
+  ugreen_nexode_einziehbares_usb_c_kabel_20_000_mah_165w_72wh,
+  ugreen_nexode_inkl_qi2_20_000_mah_145w_72wh,
+  veger_integriertes_usb_c_lightning_kabel_25_000_mah_130w_92_5wh,
+  vention_27_000_mah_140w_99_3wh,
+  vention_integriertes_usb_c_kabel_20_000_mah_165w_74wh,
+  xdigi_20_000_mah_130w_72wh,
+  xiaomi_25_000_mah_212w_90_8wh,
+  xtorm_nova_pro_20_000_mah_130w_72wh
 ];
 
 export default packs;
