c9cc34fcec63728d879a9faed19a4e9d3fc71d98
aaron yim <aaronchyim@gmail.com>
2025-12-09 12:18:29 -0600

    test: configure vitest in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/notindoors/',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test/setup.js',
+  },
 });
 
 
