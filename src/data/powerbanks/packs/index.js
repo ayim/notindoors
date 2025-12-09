b62cda35280c49fa80983b362128543b74107b3e
aaron yim <aaronchyim@gmail.com>
2025-12-09 13:00:22 -0600

    data: consolidate powerbank source and regenerate packs

---
+++ src/data/powerbanks/packs/index.js
@@ -0,0 +1,35 @@
+import anker_165w from './anker-165w.json';
+import anker_737_24k from './anker-737-24k.json';
+import anker_87w from './anker-87w.json';
+import aohi_210w_10k from './aohi-210w-10k.json';
+import aohi_310w_20k from './aohi-310w-20k.json';
+import asperx_165w from './asperx-165w.json';
+import cuktech_15_ultra_20k from './cuktech-15-ultra-20k.json';
+import eco_flow_rapid_pro_20k from './eco-flow-rapid-pro-20k.json';
+import eco_flow_rapid_pro_276k from './eco-flow-rapid-pro-276k.json';
+import eco_flow_rapid_pro_x_276k from './eco-flow-rapid-pro-x-276k.json';
+import edge_x100_27k from './edge-x100-27k.json';
+import iniu_140w from './iniu-140w.json';
+import prime_250w_276k from './prime-250w-276k.json';
+import prime_300w_26k from './prime-300w-26k.json';
+import ugreen_165w from './ugreen-165w.json';
+
+const packs = [
+  anker_165w,
+  anker_737_24k,
+  anker_87w,
+  aohi_210w_10k,
+  aohi_310w_20k,
+  asperx_165w,
+  cuktech_15_ultra_20k,
+  eco_flow_rapid_pro_20k,
+  eco_flow_rapid_pro_276k,
+  eco_flow_rapid_pro_x_276k,
+  edge_x100_27k,
+  iniu_140w,
+  prime_250w_276k,
+  prime_300w_26k,
+  ugreen_165w
+];
+
+export default packs;
