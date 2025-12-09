a9f90d755a5d703e0e7f7687d37914b8d3dad157
aaron yim <aaronchyim@gmail.com>
2025-12-09 11:50:50 -0600

    fix: include asperx charging sheet in index

--- src/data/powerbanks/charging/index.js
+++ src/data/powerbanks/charging/index.js
@@ -6,10 +6,10 @@ import ecoRapid276k from './eco-flow-rapid-pro-276k.json';
 import ecoRapidX from './eco-flow-rapid-pro-x-276k.json';
 import edgeX100 from './edge-x100-27k.json';
 import iniu140 from './iniu-140w.json';
+import asperx165 from './asperx-165w.json';
 import prime250 from './prime-250w-276k.json';
 import prime300 from './prime-300w-26k.json';
 import ugreen165 from './ugreen-165w.json';
-import asperx165 from './asperx-165w.json';
 
 const chargingSheets = [
   aohi210,
@@ -20,10 +20,10 @@ const chargingSheets = [
   ecoRapidX,
   edgeX100,
   iniu140,
+  asperx165,
   prime250,
   prime300,
   ugreen165,
-  asperx165,
 ];
 
 export default chargingSheets;
